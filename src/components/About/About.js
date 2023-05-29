0b98c1ede89692be84fe43ce557be4722d106b81
unknown <maria.rodriguez@northwestern.edu>
2023-05-28 23:19:02 -0500

    about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ function About() {
           <p className='name'>Maria Rodriguez</p>
           <p className='job'>Full Stack Web Developer</p>
           <h2 className='education'>About Me</h2>
-            <div className = 'about me'>
+            <div className = 'about-me'>
             <p>
             Hello, my name is Maria! I completed the Northwestern University Full Stack Web Development Bootcamp. Please feel free to check out my Github and Portfolio for past projects and current ones I am working ones. I have also attached my LinkedIn and Resume. Please reach out with any questions! 
             <br></br>
